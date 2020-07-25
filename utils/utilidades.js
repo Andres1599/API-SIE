832175122fbd0a2310d59895d8e7caf84639125f
Andres Higueros <higueros71@gmail.com>
2020-07-25 12:30:18 -0600

    add: agregando funciones generales para poder calcular el iva de una factura al momento de migrar las facturas del sistema viejo

--- utils/utilidades.js
+++ utils/utilidades.js
@@ -1,7 +1,26 @@
+const IVA = 0.12
+
 const RANDOM_ID = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
 };
+const CalculateIva = (total) => {
+    if (typeof total === 'number') {
+        return (total / 1.12) * IVA
+    } else {
+        return 0
+    }
+}
+const CalculateWithOutIva = (total) => {
+    if (typeof total === 'number') {
+        return (total / 1.12)
+    } else {
+        return 0
+    }
+}
 
 module.exports = {
-    RANDOM_ID
+    IVA,
+    RANDOM_ID,
+    CalculateIva,
+    CalculateWithOutIva
 }
